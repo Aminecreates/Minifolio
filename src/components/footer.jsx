971acdee34845ee5b7e081af45dce4a8e76b6725
Aminecreates <aminecreates@gmail.com>
2024-09-17 16:49:37 +0100

    mobile responsive / favicon / title

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,7 +1,21 @@
 import "../styles/footer.scss";
+import { GithubLogo, LinkSimple } from "phosphor-react";
 
 function Footer() {
-  return <div className="footer-container">© 2024 Aminecreates</div>;
+  return (
+    <div className="footer-container">
+      <p> © 2024 Aminecreates</p>
+      <div className="social-icons">
+        {" "}
+        <div className="icon-container">
+          <GithubLogo size={20} color="#a0a0a0" />
+        </div>{" "}
+        <div className="icon-container">
+          <LinkSimple size={20} color="#a0a0a0" />
+        </div>{" "}
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
